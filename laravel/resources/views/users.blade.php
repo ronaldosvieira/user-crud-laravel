706ac7f3ca80c1ac772c3e0372bb9890da6144b6
Ronaldo Vieira <ronaldoesv@gmail.com>
2016-04-16 16:51:04 -0300

    added all user fields

--- laravel/resources/views/users.blade.php
+++ laravel/resources/views/users.blade.php
@@ -18,10 +18,46 @@
 
                         <!-- User Name -->
                         <div class="form-group">
-                            <label for="user-name" class="col-sm-3 control-label">Name</label>
+                            <label for="name" class="col-sm-3 control-label">Name</label>
 
                             <div class="col-sm-6">
                                 <input type="text" name="name" id="user-name" class="form-control" value="{{ old('user') }}">
+                            </div>
+                        </div>
+                        
+                        <!-- User Email -->
+                        <div class="form-group">
+                            <label for="email" class="col-sm-3 control-label">E-mail</label>
+
+                            <div class="col-sm-6">
+                                <input type="text" name="email" id="user-email" class="form-control" value="{{ old('user') }}">
+                            </div>
+                        </div>
+                        
+                        <!-- User Birthday -->
+                        <div class="form-group">
+                            <label for="birthday" class="col-sm-3 control-label">Birthday</label>
+
+                            <div class="col-sm-6">
+                                <input type="text" name="birthday" id="user-birthday" class="form-control" value="{{ old('user') }}">
+                            </div>
+                        </div>
+                        
+                        <!-- User Occupation -->
+                        <div class="form-group">
+                            <label for="occupation" class="col-sm-3 control-label">Occupation</label>
+
+                            <div class="col-sm-6">
+                                <input type="text" name="occupation" id="user-occupation" class="form-control" value="{{ old('user') }}">
+                            </div>
+                        </div>
+                        
+                        <!-- User Notes -->
+                        <div class="form-group">
+                            <label for="notes" class="col-sm-3 control-label">Notes</label>
+
+                            <div class="col-sm-6">
+                                <input type="text" name="notes" id="user-notes" class="form-control" value="{{ old('user') }}">
                             </div>
                         </div>
 
